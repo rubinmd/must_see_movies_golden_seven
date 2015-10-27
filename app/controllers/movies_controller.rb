280cb8fc04646af2784c7560033ec56c9c47cc72
rubinmd <matt@Matthews-MBP.attlocal.net>
2015-10-27 16:36:13 -0500

    Directors
    Finished directors functionality. Could be prettier, but my update
    confirmation pages have back buttons where the target doesn’t, so maybe
    a wash.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,4 +4,54 @@ class MoviesController < ApplicationController
     @directors=Director.all
   end
 
+  def add_director
+  end
+
+  def submit_director
+    @name=params["name"]
+    @img_url=params["img_url"]
+    @dob=params["dob"]
+    @bio=params["bio"]
+    d=Director.new
+    d.name=@name
+    d.image_url=@img_url
+    d.dob=@dob
+    d.bio=@bio
+    d.save
+  end
+
+  def delete_director
+    @id=params[:id]
+    Director.find(@id).destroy
+    redirect_to("/directors")
+  end
+
+  def edit_director
+    @id=params[:id]
+    @director=Director.find(@id)
+  end
+
+  def update_director
+    @id=params[:id]
+    @name=params["name"]
+    @img_url=params["img_url"]
+    @dob=params["dob"]
+    @bio=params["bio"]
+    d=Director.find(@id)
+    d.name=@name
+    d.image_url=@img_url
+    d.dob=@dob
+    d.bio=@bio
+    d.save
+  end
+
+  def view_director
+    @id=params[:id]
+    d=Director.find(@id)
+    @name=d.name
+    @img_url=d.image_url
+    @dob=d.dob
+    @bio=d.bio
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  get("/directors",           { :controller => "movies", :action => "index_directors" })
+  get("/directors",  { :controller => "movies", :action => "index_directors" })
+  get("/add_director",  { :controller => "movies", :action => "add_director" })
+  get("/submit_director",  { :controller => "movies", :action => "submit_director" })
+  get("/delete_director/:id",  { :controller => "movies", :action => "delete_director" })
+  get("/edit_director/:id",  { :controller => "movies", :action => "edit_director" })
+  get("/update_director/:id",  { :controller => "movies", :action => "update_director" })
+  get("/view_director/:id",  { :controller => "movies", :action => "view_director" })
 end
